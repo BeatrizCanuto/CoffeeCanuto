fb90949eeface2ad45747c12076d479df7ca7398
BeatrizCanuto <BeatrizCanuto>
2022-06-27 18:48:09 -0300

    update coffee

--- asserts/javascript/script.js
+++ asserts/javascript/script.js
@@ -1,7 +1,7 @@
 var nome = window.document.getElementById('nome')
 
 function validaNome() {
-    let textoNome = document.querySelector("#textoNome");
+    let Mensagem = document.querySelector("textoNome");
     if (nome.value.length < 2) {
         textoNome.innerHTML = "Nome inválido";
         textoNome.style.color = "red";
@@ -14,7 +14,7 @@ function validaNome() {
 }
 
 function validaEmail() {
-    let textoEmail = document.querySelector("#textoEmail");
+    let textoEmail = document.querySelector("textoEmail");
     if (email.value.indexOf("@") > 0 && email.value.indexOf(".") > 0) {
         textoEmail.innerHTML = "E-mail válido";
         textoEmail.style.color = "green";
@@ -26,35 +26,25 @@ function validaEmail() {
     }
 }
 
-function validaAssunto() {
-    let textoAssunto = document.querySelector("#textoAssunto");
+function validaMensagem() {
+    let textoMensagem = document.querySelector("textoMensagem");
 
     if (assunto.value.length >= 1000) {
-        textoAssunto.innerHTML =
+        textoMensagem.innerHTML =
             "Texto é muito grande, digite no máximo 1000 caracteres";
-        textoAssunto.style.color = "red";
-        textoAssunto.style.display = "block";
-        assuntoOk = false;
+        textoMensagem.style.color = "red";
+        textoMensagem.style.display = "block";
+        mensagemOk = false;
     } else {
-        textoAssunto.style.display = "none";
-        assuntoOk = true;
+        textoMensagem.style.display = "none";
+        mensagemOk = true;
     }
 }
 
 function enviar() {
-    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+    if (nomeOk == true && emailOk == true && mensagemOk == true) {
         alert("Formulário enviado com sucesso!");
     } else {
         alert("Preencha o formulário corretamente antes de enviar...");
     }
-}
-
-function mapaZoom() {
-    mapa.style.width = "800px";
-    mapa.style.height = "600px";
-}
-
-function mapaNormal() {
-    mapa.style.width = "400px";
-    mapa.style.height = "250px";
 }
